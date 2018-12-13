d3c11c016cfee2c06849959806bb6c9682f963ee
Jingyang Zhang <jingyangzhang@Jingyangs-MBP.fios-router.home>
2018-12-12 23:12:23 -0500

    Register from and design added

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { TopBar } from './TopBar';
+import {Main} from './Main';
+import { Register } from './Register';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <TopBar />
+                <Register />
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
-import { TopBar } from './components/TopBar.js';
+import {Main} from './components/Main'
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
